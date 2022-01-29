6bab3f8c1a3ca237074666ebc69061de6ffb9972
Ben Limmer <ben@benlimmer.com>
2022-01-29 13:38:28 -0700

    JS option

---
+++ _includes/posts/send_feedback.js
@@ -0,0 +1,10 @@
+function sendFeedback(isHelpful) {
+  const pageUrl = "{{include.url}}";
+  debugger;
+
+  // TODO: sendFeedback
+
+  if (isHelpful) {
+    // TODO: show "thanks" and remove buttons
+  }
+}
